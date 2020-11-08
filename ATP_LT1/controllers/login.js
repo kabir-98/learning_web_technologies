305a04b04f708695ae748666dbc3b3c25f4d1559
Mostakimul Kabir <kabirsiam1998@gmail.com>
2020-11-08 08:45:47 +0600

    LT1

---
+++ ATP_LT1/controllers/login.js
@@ -0,0 +1,39 @@
+const express 	= require('express');
+const router 	= express.Router();
+
+router.get('/', (req, res)=>{
+	res.render('login/index');
+});
+
+router.post('/', (req, res)=>{
+	var userlist = [
+		['1', 'alamin', 'abc@gmail.com', '123'],
+		['2', 'pqr', 'pqr@gmail.com', '123'],
+		['3', 'xyz', 'xyz@gmail.com', '123'],
+		['4','nur','nur@gmail.com','1']
+	];
+	req.session.userlist = userlist;
+	req.session.uid = '4';
+	userlist = req.session.userlist;
+	var loggedin = false;
+
+	userlist.forEach(function(user){
+		if(req.body.username == user[1] && req.body.password == user[3]){
+			loggedin = true;
+		}
+	});
+
+	if(loggedin){
+		res.cookie('uname', req.body.username);
+		res.redirect('/home');
+
+	}else{
+		res.redirect('/login');
+	}
+}); 
+
+
+module.exports = router;
+
+
+
